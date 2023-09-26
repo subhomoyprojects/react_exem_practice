d8157b6ff466a3394f4cda5175fa08f1ef32cde0
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-09-27 00:42:03 +0530

    add practice

--- src/Redux/Slice/AuthSlice.js
+++ src/Redux/Slice/AuthSlice.js
@@ -1,6 +1,12 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import instance from "../Helper";
 
+export const status = Object.freeze({
+  success: "SUCCESS",
+  loading: "LOADING",
+  error: "ERROR",
+});
+
 export const authRegister = createAsyncThunk("/user/signup", async (formData) => {
   let res = instance.post(`/user/signup`, formData);
   let resData = res?.data;
@@ -9,9 +15,22 @@ export const authRegister = createAsyncThunk("/user/signup", async (formData) =>
 
 const authSlice = createSlice({
   name: "authSlice",
-  initialState: {},
+  initialState: {
+    status: status.success,
+  },
   reducers: {},
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(authRegister.pending, (state) => {
+        state.status = status.loading;
+      })
+      .addCase(authRegister.fulfilled, (state) => {
+        state.status = status.success;
+      })
+      .addCase(authRegister.rejected, (state) => {
+        state.status = status.error;
+      });
+  },
 });
 
 export default authSlice.reducer;
